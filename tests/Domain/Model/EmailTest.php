1e0c16e8f74809bd54d8ee193c565f57f50aef3a
jose <jose.cuellar.villegas@gmail.com>
2017-11-07 23:01:09 +0100

    first commit
    first commit

---
+++ tests/Domain/Model/EmailTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Wphp\Domain\Model;
+
+/**
+ * @covers Email
+ */
+class EmailTest extends \PHPUnit_Framework_TestCase
+{
+    public function testCanBeCreatedFromValidEmailAddress(): void
+    {
+        $this->assertInstanceOf(
+            Email::class,
+            Email::fromString('user@example.com')
+        );
+    }
+
+    public function testCannotBeCreatedFromInvalidEmailAddress(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+
+        Email::fromString('');
+    }
+
+    public function testCanBeUsedAsString(): void
+    {
+        $this->assertEquals(
+            'user@example.com',
+            Email::fromString('user@example.com')
+        );
+    }
+}
